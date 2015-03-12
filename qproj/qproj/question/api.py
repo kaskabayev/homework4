68b6a0b5d832dde7b6b5233553b9bee8b683aba8
Madiyar <madiyar.kaskabayev@cs.sdu.edu.kz>
2015-03-13 02:17:41 +0600

    Commit

--- qproj/qproj/question/api.py
+++ qproj/qproj/question/api.py
@@ -3,12 +3,25 @@ from qproj.question import models
 from tastypie.authorization import Authorization
 from tastypie import fields
 
-class QuestionResource(ModelResource):
-	text = fields.CharField(attribute="text", use_in="list")
-	is_public = fields.BooleanField(attribute="is_public", use_in="detail")
-	
-	class Meta:
-		queryset = models.Question.objects.all()
-		resource_name = 'question'
-		authorization = Authorization()
-		always_return_data = True
+class PostResource(ModelResource):
+    author = fields.CharField(attribute = "author");
+    title = fields.CharField(attribute = "title");
+    text = fields.CharField(attribute = "text");
+    comment = fields.ToManyField('qproj.question.api.CommentResource', 'comment_set', null = True, use_in = "detail", full=True);
+
+    class Meta:
+        queryset = models.Post.objects.all()
+        resource_name = 'post'
+        authorization = Authorization()
+        always_return_data = True
+
+class CommentResource(ModelResource):
+    author = fields.CharField(attribute = "author", use_in = "list")
+    text = fields.CharField(attribute = "text", use_in = "list")
+    post = fields.ToOneField('qproj.question.api.PostResource', 'post')
+
+    class Meta:
+        queryset = models.Comment.objects.all()
+        resource_name = 'comment'
+        authorization = Authorization()
+        always_return_data = True
--- qproj/qproj/question/models.py
+++ qproj/qproj/question/models.py
@@ -1,7 +1,16 @@
-from django.db import models
 from django.utils import timezone
+from django.db import models
 
-class Question(models.Model):
-	text = models.CharField(max_length=1000)
-	pub_time = models.DateTimeField(auto_now_add=timezone.now)
-	is_public = models.BooleanField(default=False)
+class Post(models.Model):
+	author = models.CharField(max_length = 1000)
+	title = models.CharField(max_length = 1000)
+	text = models.CharField(max_length = 1000)
+	pub_date = models.DateTimeField(auto_now_add = timezone.now)
+	upd_date = models.DateTimeField(auto_now = timezone.now)
+	is_public = models.BooleanField(default = True)
+
+class Comment(models.Model):
+	author = models.CharField(max_length = 1000)
+	text = models.CharField(max_length = 1000)
+	pub_date = models.DateTimeField(auto_now_add = timezone.now)
+	post = models.ForeignKey(Post)
